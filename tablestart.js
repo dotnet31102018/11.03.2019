283ac0e9f009637a4faddfb8d648b974f32969db
app182019 <47757230+app182019@users.noreply.github.com>
2019-03-11 19:22:09 +0200

    Create tablestart.js

---
+++ tablestart.js
@@ -0,0 +1,43 @@
+            <!DOCTYPE html>
+            <html>
+            <head>
+            </head>
+            <body>                                    
+              <style>
+                table{
+                  width: 100%;
+                  margin: 25px 0;
+                  border-collapse: collapse;
+                }
+                table, th, td{
+                  border: 1px solid #6C220B;
+                }
+                table th, table td{
+                  padding: 8px;
+                  text-align: left;
+                }
+             </style>
+             <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      
+             <form>
+                <input type="text" id="name" placeholder="Enter Name">
+                <input type="text" id="subject" placeholder="Enter Subject">
+                <input type="button" id="btn1" value="Click to Add Row">
+             </form>
+             <table>
+                <thead>
+                   <tr>
+                      <th>Name</th>
+                      <th>Subject</th>
+                   </tr>
+                </thead>
+                <tbody>
+                   <tr>
+                      <td>Itay</td>
+                      <td>Java Script</td>
+                   </tr>
+                </tbody>
+             </table>              
+            </body>
+            </html>   
+        
